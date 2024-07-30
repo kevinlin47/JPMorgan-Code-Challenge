a37f60eb71410cf5b19d77192f531ec788246572
kevin <kevinlin47@hotmail.com>
2024-07-30 18:27:07 -0400

    Update lambda code

--- lambda-code/lambda_handler.py
+++ lambda-code/lambda_handler.py
@@ -8,7 +8,8 @@ import json
 import urllib.parse
 import boto3
 
-s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
+dynamodb = boto3.client("dynamodb")
 
 def lambda_handler(event, context):
     print("Received event: " + event['Records'][0]['eventName'])
@@ -20,10 +21,47 @@ def lambda_handler(event, context):
     print("Object Key: " + key)
 
     try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        return response['ContentType']
+        response = s3.Bucket(bucket).Object(key)
+       
+        movie_data = response.get()['Body'].read().decode('utf-8')
+        json_data = json.loads(movie_data)
+
+        # Cast and Genres will be stored as a List of Strings
+        cast_members = []
+        genres = []
+
+        for cast in json_data[0]["cast"]:
+          cast_members.append({"S": cast})
+
+        for genre in json_data[0]["genres"]:
+          genres.append({"S": genre})
+        
+        movie = {
+          "Title"           : {'S': json_data[0]["title"]},
+          "Year"            : {'N': str(json_data[0]["year"])},
+          "Cast"            : {'L': cast_members},
+          "Genres"          : {'L': genres},
+          "Href"            : {'S': json_data[0]["href"]},
+          "Extract"         : {'S': json_data[0]["extract"]},
+          "Thumbnail"       : {'S': json_data[0]["thumbnail"]},
+          "Thumbnail_width" : {'N': str(json_data[0]["thumbnail_width"])},
+          "Thumbnail_height": {'N': str(json_data[0]["thumbnail_height"])}
+        }
+
+        print("The following movie data will be put into the database\n")
+        print(movie)
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
+    
+    try:
+      # If a movie with the given name already exists, it will be updated
+      print("Executing database put_item\n")
+      print("If an existing entry is updated, the old attribute values will be returned below\n")
+      print(dynamodb.put_item(TableName='Movies', Item=movie, ReturnValues="ALL_OLD")["Attributes"])
+
+    except Exception as e:
+      print(e)
+      print("Error when putting item into Table Movies")
+      raise e
